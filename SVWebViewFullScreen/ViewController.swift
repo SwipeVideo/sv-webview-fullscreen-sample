13d6e1012c180ce8bd2e45b92175b437dbda6768
nakazonot <nakazono@amatelus.co.jp>
2020-09-17 13:30:14 +0900

    Initial Commit

---
+++ SVWebViewFullScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SVWebViewFullScreen
+//
+//  Created by taichi nakazono on 2020/09/17.
+//  Copyright © 2020 nakazono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
